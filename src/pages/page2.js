905b957a9be1d646732945a3818502e383a8848d
Nick9499 <45527115+Nick9499@users.noreply.github.com>
2021-07-17 21:48:57 +0530

    final2

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
-import Chart from "react-google-charts"
+
 import { useHistory } from "react-router-dom"
 import { PieChart } from "react-minimal-pie-chart"
 
